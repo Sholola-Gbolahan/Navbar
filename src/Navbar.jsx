a56e9fbb05a9f547c4942ace4dda8d5c401eb14a
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-07-12 14:36:26 +0100

    Add styling do proper display on large screen

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,28 +1,35 @@
-import React, { useState } from "react"
+import React, { useState, useRef, useEffect } from "react"
 import { links, social } from "./data"
 import { FaBars } from "react-icons/fa"
 import logo from "./logo.svg"
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false)
+  const linksRef = useRef(null)
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks)
+  }
+
+  const linkStyle = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : "0px",
+  }
 
   return (
     <nav>
       <div className="nav-center">
+        {/* Nav Header */}
         <div className="nav-header">
           <img src={logo} className="logo" alt="header-image" />
-          <button
-            onClick={() => setShowLinks(!showLinks)}
-            className="nav-toggle"
-          >
+          <button onClick={toggleLinks} className="nav-toggle">
             <FaBars />
           </button>
         </div>
-        <div
-          className={
-            showLinks ? "links-container show-container" : "links-container"
-          }
-        >
-          <ul className="links">
+
+        {/* Nav Links */}
+        <div className="links-container" style={linkStyle}>
+          <ul className="links" ref={linksRef}>
             {links.map(({ id, url, text }) => {
               return (
                 <li key={id}>
@@ -32,6 +39,17 @@ const Navbar = () => {
             })}
           </ul>
         </div>
+
+        {/* Social Links */}
+        <ul className="social-icons">
+          {social.map(({ id, url, icon }) => {
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </nav>
   )
